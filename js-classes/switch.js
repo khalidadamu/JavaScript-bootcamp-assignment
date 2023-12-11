0765676b7f30c8edeedee86250ce6995be10d6d4
Khalid <khalidbox720@icloud.com>
2023-12-11 11:51:45 +0100

    worked on while loops and switch

---
+++ js-classes/switch.js
@@ -0,0 +1,93 @@
+"use strict";
+
+// const grade = "A";
+
+// console.log("Entrring switch block");
+// switch (grade) {
+//   case "A":
+//     console.log("Good Job");
+//     break;
+//   case "B":
+//     console.log("Pretty Good Job");
+//     break;
+//   case "C":
+//     console.log("Passed");
+//     break;
+//   default:
+//     console.log("bad job");
+// }
+
+// const day = "Monday";
+
+// switch (day) {
+//   case "Monday":
+//     console.log("Wear red shirt");
+//     break;
+//   case "Tuesday":
+//     console.log("Wear blue shirt");
+//     break;
+//   case "Wenesday":
+//     console.log("Wear yellow shirt");
+//     break;
+//   case "Thursday":
+//     console.log("Wear white shirt");
+//     break;
+//   case "Friday":
+//     console.log("Native");
+//     break;
+
+//   default:
+//     console.log("kkkkjsjjsjjs");
+// }
+
+// const fruitType = "berries";
+
+// switch (fruitType) {
+//   case "Orange":
+//     console.log("Orange are $5 a pound");
+//     break;
+//   case "Apples":
+//     console.log("Apples are $5 a pound");
+//     break;
+//   case "Mango":
+//     console.log("Mango are $5 a pound");
+//     break;
+//   case "Pineapple":
+//     console.log("Pineapple are $5 a pound");
+//     break;
+//   default:
+//     console.log(`Sorry we are of fruit type ${fruitType}`);
+// }
+
+// Asignment
+// Write a script to welcome yout guset to your vent based on thier type of ticket
+const ticket = "Regular";
+
+switch (ticket) {
+  case "regular":
+    console.log(`You have regular ticket`);
+    break;
+  case "VIP":
+    console.log(`You have VIP ticket`);
+    break;
+  case "VVIP":
+    console.log(`You have VVIP ticket`);
+    break;
+  case "GuestHonour":
+    console.log(`You have guestHonour ticket`);
+    break;
+  default:
+    console.log(`You have unknown ticket`);
+}
+
+// While Loops
+
+// while (true) {
+//      statement
+// }
+
+let x = 0;
+while (x < 10) {
+  x++;
+  console.log(x);
+}
