e9b6822382dc14c266d3d4a563c261156b29ee23
khalidadamu <khalidbox720@icloud.com>
2023-12-15 22:16:29 +0100

    more data structure

--- js-classes/function.js
+++ js-classes/function.js
@@ -91,3 +91,52 @@ const myGenerator = numberGenerator(1, 5);
 for (const number of myGenerator) {
   console.log(number);
 }
+
+
+
+
+function swapLetter(text) {
+  let x = text.split("");
+
+  for (let i = 0; i < x.length; i++) {
+    switch (x[i]) {
+      case "":
+        break;
+      case "z":
+        x[i] = "a";
+        break;
+      case "Z":
+        x[i] = "A";
+        break;
+      default:
+        x[i] = String.fromCharCode(1 + x[i].charCodeAt(0));
+    }
+  }
+  return x.join("");
+}
+
+console.log(swapLetter("zoology"));
+
+// Luthor Question
+// Write a script that changes the case of words with at least two character / letter to words in uppper case or lower case. A case instance where "drink" will change to drink and "Map" will change to "MAP"
+
+function caseChanger(charInput) {
+  let upperCharNum = 0;
+  let lowerCharNum = 0;
+
+  for (let i = 0; i < charInput.length; i++) {
+    if (/[A-Z]/.test(charInput[i])) {
+      upperCharNum++;
+    } else {
+      lowerCharNum++;
+    }
+  }
+
+  if (upperCharNum > lowerCharNum) {
+    return charInput.toUpperCase();
+  } else {
+    return charInput.toLowerCase();
+  }
+}
+
+console.log(caseChanger("loWER"));
